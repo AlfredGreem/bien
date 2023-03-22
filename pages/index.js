38bcf850f52c6a7fddb4c2f2ba6acff8d6b264d6
Alfred Obi <houseofintellecte@gmail.com>
2023-03-22 18:13:36 +0100

    Initialization

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello There!
+    </h1>
+    </div>
+  )
+}
